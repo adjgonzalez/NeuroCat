b6eb7558aa2ab1614267ab1bd46d9fb83df1ee45
adjgonzalez <adjgonzalez@mun.ca>
2026-06-26 18:46:15 -0230

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyBLG3MR-2L_fSKTBHCLGhGJ73JzK-bRGOQ",
+  authDomain: "neurocat-c8511.firebaseapp.com",
+  projectId: "neurocat-c8511",
+  storageBucket: "neurocat-c8511.firebasestorage.app",
+  messagingSenderId: "384380011734",
+  appId: "1:384380011734:web:e4e9e30c3d92ea9037f577",
 };
 
 export const firestorePath = {
